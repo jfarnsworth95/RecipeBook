5a7e264b16022f36b5b032b6bd9162db249e8d74
jfarnsworth95 <jafarns95@gmail.com>
2022-04-10 23:05:05 -0500

    Added file IO for internal/external storage. No google cloud CRUD added yet

--- app/src/main/java/com/jaf/recipebook/Helper.java
+++ app/src/main/java/com/jaf/recipebook/Helper.java
@@ -2,23 +2,117 @@ package com.jaf.recipebook;
 
 import android.content.Context;
 import android.content.SharedPreferences;
+import android.net.Uri;
 import android.os.Environment;
+import android.os.ParcelFileDescriptor;
+import android.util.Log;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.Objects;
 
 public class Helper {
 
     public SharedPreferences appPreferences;
     public Context context;
 
+    public final String TAG = "JAF-HELPER";
+
     public Helper(Context context){
         this.context = context;
-        appPreferences = getSharedPreferences();
+        appPreferences = context.getSharedPreferences(context.getString(R.string.preference_file_key),
+                Context.MODE_PRIVATE);
     }
 
-    public SharedPreferences getSharedPreferences(){
-        return context.getSharedPreferences(context.getString(R.string.preference_file_key),
-                                                              Context.MODE_PRIVATE);
+    public Uri[] getAllFileUris(){
+        File[] files;
+        if(Environment.isExternalStorageManager()){
+            // Fetch all external app files
+            files = context.getExternalFilesDir(null).listFiles();
+        } else {
+            // Fetch all internal app files
+            files = context.getFilesDir().listFiles();
+        }
+
+        Uri[] uris = new Uri[files.length];
+        for (int x = 0; x < files.length; x ++){
+            uris[x] = Uri.fromFile(files[x]);
+        }
+        return uris;
     }
-    
+
+    public Uri getFileUri(String filename){
+        File file;
+        if(Environment.isExternalStorageManager()){
+            // Fetch external app file
+            Log.i(TAG, "getFileUri: Grabbing External");
+            file = new File(context.getExternalFilesDir(null), filename);
+        } else {
+            // Fetch internal app file
+            Log.i(TAG, "getFileUri: Grabbing Internal");
+            file = new File(context.getFilesDir(), filename);
+        }
+        return Uri.fromFile(file);
+    }
+
+    public boolean deleteFile(String filename){
+        File file;
+        if(Environment.isExternalStorageManager()){
+            // Fetch external app file
+            file = new File(context.getExternalFilesDir(null), filename);
+        } else {
+            // Fetch internal app file
+            file = new File(context.getFilesDir(), filename);
+        }
+        return file.delete();
+    }
+
+    public String readFileUri(Uri uri) throws IOException {
+        StringBuilder stringBuilder = new StringBuilder();
+        try (InputStream inputStream =
+                     context.getContentResolver().openInputStream(uri);
+             BufferedReader reader = new BufferedReader(
+                     new InputStreamReader(Objects.requireNonNull(inputStream)))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                stringBuilder.append(line);
+            }
+        }
+        return stringBuilder.toString();
+    }
+
+    public void editFileUri(Uri uri, String newContent) {
+        try {
+            ParcelFileDescriptor pfd = context.getContentResolver().
+                    openFileDescriptor(uri, "w");
+
+            FileOutputStream fileOutputStream =
+                    new FileOutputStream(pfd.getFileDescriptor());
+            fileOutputStream.write(newContent.getBytes());
+
+            fileOutputStream.close();
+            pfd.close();
+        } catch (FileNotFoundException e) {
+            Log.e(TAG, "editFileUri: File not found", e);
+            e.printStackTrace();
+        } catch (IOException e) {
+            Log.e(TAG, "editFileUri: IO Exception", e);
+            e.printStackTrace();
+        }
+    }
+
+    public void createFileUri(String filename, String content){
+        Log.i(TAG, "createFileUri: creating file...");
+        Uri uri = getFileUri(filename);
+        editFileUri(uri, content);
+    }
+
+    public boolean doesFileExist(String filename){
+        return new File(getFileUri(filename).getPath()).exists();
+    }
 }
