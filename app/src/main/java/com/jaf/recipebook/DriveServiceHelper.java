d900468a5e888e99457c72c6119f133d284fca58
jfarnsworth95 <jafarns95@gmail.com>
2022-04-24 20:28:27 -0500

    Removing License info since I had to change the base class and method implementation so damn much

--- app/src/main/java/com/jaf/recipebook/DriveServiceHelper.java
+++ app/src/main/java/com/jaf/recipebook/DriveServiceHelper.java
@@ -1,19 +1,4 @@
 package com.jaf.recipebook;
-/**
- * Copyright 2018 Google LLC
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 
 import android.content.ContentResolver;
 import android.content.Context;
@@ -89,11 +74,7 @@ public class DriveServiceHelper {
         return googleDriveService;
     }
 
-    /**
-     * Creates a text file in the user's My Drive folder and returns its file ID.
-     */
     public Task<File> createFile(String filename) {
-
         final TaskCompletionSource<File> tcs = new TaskCompletionSource<File>();
         mExecutor.execute(
                 new Runnable() {
@@ -627,8 +608,7 @@ public class DriveServiceHelper {
      * Opens the file at the {@code uri} returned by a Storage Access Framework {@link Intent}
      * created by {@link #createFilePickerIntent()} using the given {@code contentResolver}.
      */
-    public Task<Pair<String, String>> openFileUsingStorageAccessFramework(
-            ContentResolver contentResolver, Uri uri) {
+    public Task<Pair<String, String>> openFileUsingStorageAccessFramework(ContentResolver contentResolver, Uri uri) {
         final TaskCompletionSource<Pair<String, String>> tcs = new TaskCompletionSource<Pair<String, String>>();
         mExecutor.execute(
                 new Runnable() {
